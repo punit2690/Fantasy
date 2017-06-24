c692dde0952e242508ac2da91b7ee18156cb95d1
Punit Kulkarni <punit.2690@yahoo.com>
2017-06-24 14:10:21 -0400

    Initial Commit

---
+++ CBS Fantasy/Main Front/FrontsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FrontsViewController.swift
+//  CBS Fantasy
+//
+//  Created by Punit Kulkarni on 6/24/17.
+//  Copyright © 2017 Punit Kulkarni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FrontsViewController: UIViewController {
+    
+}
